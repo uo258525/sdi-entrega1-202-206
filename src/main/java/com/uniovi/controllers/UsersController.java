d5e5b101c78c0b45464e97c5de6fb5e01f3c44f9
astr <UO258194@uniovi.es>
2019-03-05 20:10:54 +0100

    Adding controllers User and Home

---
+++ src/main/java/com/uniovi/controllers/UsersController.java
@@ -0,0 +1,60 @@
+package com.uniovi.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.uniovi.entities.User;
+import com.uniovi.services.SecurityService;
+import com.uniovi.services.UserService;
+import com.uniovi.validators.SignUpFormValidator;
+
+@Controller
+public class UsersController {
+	@Autowired
+	private UserService usersService;
+	
+	@Autowired
+	private SignUpFormValidator signUpFormValidator;
+	
+	@Autowired
+	private SecurityService securityService;
+	
+	@RequestMapping(value = "/signup", method = RequestMethod.GET)
+	public String signup(Model model) {
+		model.addAttribute("user", new User());
+		return "signup";
+	}
+
+	@RequestMapping(value = "/signup", method = RequestMethod.POST)
+	public String signup(@Validated User user, BindingResult result, Model model) {
+		signUpFormValidator.validate(user, result);
+		if (result.hasErrors()) {
+			return "signup";
+		}
+		
+		usersService.addUser(user);
+		securityService.autoLogin(user.getEmail(), user.getPassword2());
+		return "redirect:home";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login(Model model) {
+		return "login";
+	}
+
+	@RequestMapping(value = { "/home" }, method = RequestMethod.GET)
+	public String home(Model model) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String email = auth.getName();
+		User activeUser = usersService.getUserByEmail(email);
+		
+		return "home";
+	}
+}
